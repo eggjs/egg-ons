9e415657a10a64fe28eb42271009686b2afbf9c9
dead-horse <dead_horse@qq.com>
2018-07-26 13:21:03 +0800

    refactor: use async await

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const ONS = require('./lib/ons');
 
 module.exports = app => {
   app.ons = new ONS(app);
-  app.beforeStart(function* () {
-    yield app.ons.init();
+  app.beforeStart(async () => {
+    await app.ons.init();
   });
 };
